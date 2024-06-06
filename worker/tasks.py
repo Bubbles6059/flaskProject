be6417f81dfeb08e65ebb51a8b153fdc3759a29f
Bubblie123 <rohan.suresh712@gmail.com>
2024-06-06 16:53:52 +0530

    Quick fix with unfinished docker compose

--- worker/tasks.py
+++ worker/tasks.py
@@ -1,30 +1,8 @@
 from celery import Celery, shared_task
 import time
 
-def celery_init_app(app: Flask) -> Celery:
-    class FlaskTask(Task):
-        def __call__(self, *args: object, **kwargs: object) -> object:
-            with app.app_context():
-                return self.run(*args, **kwargs)
-
-    celery_app = Celery(app.name, task_cls=FlaskTask)
-    celery_app.config_from_object(app.config["CELERY"])
-    celery_app.set_default()
-    app.extensions["celery"] = celery_app
-    return celery_app
-
-def create_app() -> Flask:
-    app = Flask(__name__)
-    app.config.from_mapping(
-        CELERY=dict(
-            broker_url="redis://localhost",
-            result_backend="redis://localhost",
-            task_ignore_result=True,
-        ),
-    )
-    app.config.from_prefixed_env()
-    celery_init_app(app)
-    return app
+from flask import Flask
+from config import create_app
 
 flask_app = create_app()
 celery_app = flask_app.extensions["celery"]
